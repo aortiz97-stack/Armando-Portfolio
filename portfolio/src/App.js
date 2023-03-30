bb8c8a3f12a36356f4b2727577da4e7656825f19
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-30 15:11:04 -0500

    Format profile and brief description in upper header

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import armandoImg from './images/armando_profile-modified.jpeg'
 
 function App() {
   return (
     <div>
+      <header>
+        <div className="centering-container">
+          <img src={armandoImg} alt=""/>
+          <p>Armando Ortiz</p>
+          <p>Aspiring Full-Stack Developer</p>
+        </div>
+      </header>
+
+      <main></main>
+      <footer></footer>
     </div>
   );
 }
