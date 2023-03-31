bf4c93ba72c817672060e60ddcc1642f0b468cf3
Armando <armando.ortiz.jr.19@outlook.com>
2023-03-31 14:11:28 -0500

    Add skeletal structure for project grid

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,17 +5,107 @@ function App() {
   return (
     <div>
       <header>
-        <div className="centering-container">
-          <img src={armandoImg} alt=""/>
-          <p>Armando Ortiz</p>
-          <p>Aspiring Full-Stack Developer</p>
-        </div>
+        <img src={armandoImg} alt=""/>
+        <h1>Armando Ortiz</h1>
+        <p>Aspiring Full-Stack Developer</p>
       </header>
+      <main>
+        <section id="about-me">
+          <h2>About Me</h2>
+          <p>I am a self-taught programmer with skills in frontend design.</p>
+        </section>
 
-      <main></main>
+        <section id="projects">
+          <h2>Projects</h2>
+          <div className="grid-img-container">
+            <div className="project">
+              <div className="screenshot-container">Screenshot</div>
+              <div className="project-description-container">
+                <div className="project-top-portion">
+                  <h3>Project Name</h3>
+                  <div className="project-link-icons">Icons here</div>
+                </div>
+                <p>Short description</p>
+              </div>
+            </div>
+
+            <div className="project">
+              <div className="screenshot-container">Screenshot</div>
+              <div className="project-description-container">
+                <div className="project-top-portion">
+                  <h3>Project Name</h3>
+                  <div className="project-link-icons">Icons here</div>
+                </div>
+                <p>Short description</p>
+              </div>
+            </div>
+
+            <div className="project">
+              <div className="screenshot-container">Screenshot</div>
+              <div className="project-description-container">
+                <div className="project-top-portion">
+                  <h3>Project Name</h3>
+                  <div className="project-link-icons">Icons here</div>
+                </div>
+                <p>Short description</p>
+              </div>
+            </div>
+
+            <div className="project">
+              <div className="screenshot-container">Screenshot</div>
+              <div className="project-description-container">
+                <div className="project-top-portion">
+                  <h3>Project Name</h3>
+                  <div className="project-link-icons">Icons here</div>
+                </div>
+                <p>Short description</p>
+              </div>
+            </div>
+
+            <div className="project">
+              <div className="screenshot-container">Screenshot</div>
+              <div className="project-description-container">
+                <div className="project-top-portion">
+                  <h3>Project Name</h3>
+                  <div className="project-link-icons">Icons here</div>
+                </div>
+                <p>Short description</p>
+              </div>
+            </div>
+
+            <div className="project">
+              <div className="screenshot-container">Screenshot</div>
+              <div className="project-description-container">
+                <div className="project-top-portion">
+                  <h3>Project Name</h3>
+                  <div className="project-link-icons">Icons here</div>
+                </div>
+                <p>Short description</p>
+              </div>
+            </div>
+          </div>
+
+          
+
+        </section>
+      </main>
       <footer></footer>
     </div>
   );
 }
 
 export default App;
+
+/*<header>
+          <div className="img-container"><img src={armandoImg} alt=""/></div>
+          <h1>Armando Ortiz</h1>
+          <p>Aspiring Full-Stack Developer</p>
+
+      </header>
+      <main>
+        <section id="about-me">
+          <div className="centering-container">
+            <h2>About Me</h2>
+          </div>
+        </section>
+      </main>*/
