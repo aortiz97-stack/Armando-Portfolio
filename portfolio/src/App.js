956db52bb14352dd666c4d6667fccabef6052610
Armando <armando.ortiz.jr.19@outlook.com>
2023-04-06 14:12:02 -0500

    Add links to project buttons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,10 @@ import './App.css';
 import armandoImg from './images/armando_profile-modified.jpeg'
 import shoppingCartImg from './images/shopping-cart-project.png';
 import goToLink from './images/go-to-link.png';
+import memoryCardImg from './images/Memory-Card-Game.png';
+import calculatorImg from './images/calculator-project.png';
+import ticTacToeImg from './images/tic-tac-toe.png';
+import todoListImg from './images/todo-list-project.png';
 
 function App() {
   return (
@@ -28,8 +32,8 @@ function App() {
                 <div className="project-top-portion">
                   <h3>Lemongrab's Online Shop</h3>
                   <div className="project-link-icons">
-                    <button><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" className="logo" /></button>
-                    <button><img src={goToLink} alt="External link icon" className="logo" /></button>
+                    <button><a href='https://github.com/aortiz97-stack/Shopping-Cart' target='_blank' rel="noreferrer"><img src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' alt="GitHub" className="logo" /></a></button>
+                    <button><a href="https://aortiz97-stack.github.io/Shopping-Cart/" target='_blank' rel="noreferrer"><img src={goToLink} alt="External link icon" className="logo" /></a></button>
                   </div>
                 </div>
                 <p className="project-description-text">Shopping cart app inspired by Adventure Time</p>
@@ -38,50 +42,65 @@ function App() {
 
             <div className="project">
               <div className="screenshot-container">
-                <img src={shoppingCartImg} alt="" className="project-screenshot" />
+                <img src={memoryCardImg} alt="" className="project-screenshot" />
               </div>
               <div className="project-description-container">
                 <div className="project-top-portion">
-                  <h3>Lemongrab's Online Shop</h3>
+                  <h3>Memory Card Game</h3>
                   <div className="project-link-icons">
-                    <button><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" className="logo" /></button>
-                    <button><img src={goToLink} alt="External link icon" className="logo" /></button>
+                    <button><a href="https://github.com/aortiz97-stack/Memory-Card" target="_blank" rel="noreferrer"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" className="logo" /></a></button>
+                    <button><a href="https://aortiz97-stack.github.io/Memory-Card/" target='_blank' rel="noreferrer"><img src={goToLink} alt="External link icon" className="logo" /></a></button>
                   </div>
                 </div>
-                <p className="project-description-text">Shopping cart app inspired by Adventure Time</p>
+                <p className="project-description-text">Memory card game that challenges you not to pick the same card</p>
               </div>
             </div>
 
             <div className="project">
-              <div className="screenshot-container">Screenshot</div>
+              <div className="screenshot-container">
+                <img src={calculatorImg} alt="" className="project-screenshot" />
+              </div>
               <div className="project-description-container">
                 <div className="project-top-portion">
-                  <h3>Project Name</h3>
-                  <div className="project-link-icons">Icons here</div>
+                  <h3>Calculator App</h3>
+                  <div className="project-link-icons">
+                    <button><a href="https://github.com/aortiz97-stack/TODP-calculator" target='_blank' rel="noreferrer"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" className="logo" /></a></button>
+                    <button><a href="https://aortiz97-stack.github.io/TODP-calculator/" target='_blank' rel="noreferrer"><img src={goToLink} alt="External link icon" className="logo" /></a></button>
+                  </div>
                 </div>
-                <p>Short description</p>
+                <p className="project-description-text">Simple calculator with similar interface with iPhone calcualtor</p>
               </div>
             </div>
 
             <div className="project">
-              <div className="screenshot-container">Screenshot</div>
+              <div className="screenshot-container">
+                <img src={todoListImg} alt="" className="project-screenshot" />
+              </div>
               <div className="project-description-container">
                 <div className="project-top-portion">
-                  <h3>Project Name</h3>
-                  <div className="project-link-icons">Icons here</div>
+                  <h3>Todo List App</h3>
+                  <div className="project-link-icons">
+                    <button><a href="https://github.com/aortiz97-stack/TODO-List-TODP" target='_blank' rel="noreferrer"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" className="logo" /></a></button>
+                    <button><a href="https://aortiz97-stack.github.io/TODO-List-TODP/" target='_blank' rel="noreferrer"><img src={goToLink} alt="External link icon" className="logo" /></a></button>
+                  </div>
                 </div>
-                <p>Short description</p>
+                <p className="project-description-text">Todo app that records to-do activities (backend coming soon)</p>
               </div>
             </div>
 
             <div className="project">
-              <div className="screenshot-container">Screenshot</div>
+              <div className="screenshot-container">
+                <img src={ticTacToeImg} alt="" className="project-screenshot" />
+              </div>
               <div className="project-description-container">
                 <div className="project-top-portion">
-                  <h3>Project Name</h3>
-                  <div className="project-link-icons">Icons here</div>
+                  <h3>Tic-Tac-Toe Game</h3>
+                  <div className="project-link-icons">
+                    <button><a href="https://github.com/aortiz97-stack/Tic-Tac-Toe-TODP" target='_blank' rel="noreferrer"><img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" className="logo" /></a></button>
+                    <button><a href="https://aortiz97-stack.github.io/Tic-Tac-Toe-TODP/" target='_blank' rel="noreferrer"><img src={goToLink} alt="External link icon" className="logo" /></a></button>
+                  </div>
                 </div>
-                <p>Short description</p>
+                <p className="project-description-text">A simple game of Tic-Tac-Toe</p>
               </div>
             </div>
 
@@ -96,9 +115,6 @@ function App() {
               </div>
             </div>
           </div>
-
-          
-
         </section>
       </main>
       <footer></footer>
